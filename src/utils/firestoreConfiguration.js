784a8c8f100c11f39f8ffb4b9a423b56638ce47f
sebastian <sebastian.dorsch@4rsoluciones.com>
2022-11-24 20:50:30 -0300

    constants and environment

--- src/utils/firestoreConfiguration.js
+++ src/utils/firestoreConfiguration.js
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { Constants } from "../constants"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSnynA-Jg8vy9RccK9DlJPi6xRDsRmqKs",
-  authDomain: "ecommerce-b01e5.firebaseapp.com",
-  projectId: "ecommerce-b01e5",
-  storageBucket: "ecommerce-b01e5.appspot.com",
-  messagingSenderId: "89627408876",
-  appId: "1:89627408876:web:f325a7835b6f2b5466e8c9"
+  apiKey: `${Constants.apiKey}`,
+  authDomain: `${Constants.authDomain}`,
+  projectId: `${Constants.projectId}`,
+  storageBucket: `${Constants.storageBucket}`,
+  messagingSenderId: `${Constants.messagingSenderId}`,
+  appId: `${Constants.appId}`
 }; 
 
 // Initialize Firebase
